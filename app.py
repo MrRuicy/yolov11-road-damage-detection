6240d5d8c752a2e1c6a5e68aeee58a21e0dde236
MrRuicy <128280863+MrRuicy@users.noreply.github.com>
2025-03-06 20:48:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+import sys
+import subprocess
+
+print("Python version:", sys.version)
+print("Python executable:", sys.executable)
+
+# 检查 OpenCV 是否安装
+try:
+    import cv2
+    print("OpenCV version:", cv2.__version__)
+except ModuleNotFoundError:
+    print("cv2 module not found, trying to install...")
+    subprocess.run(["pip", "install", "opencv-python-headless"])
+
+
 import streamlit as st
 import numpy as np
 from PIL import Image
